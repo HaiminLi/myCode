24bb1580f932059ca10abda56656f4cde26ccf2e
HaiminLi <1073780639@qq.com>
2020-01-20 16:10:29 +0800

    G1

--- yancheng/spring-anything/src/main/java/com/haimin/li/jvm/MyJVM.java
+++ yancheng/spring-anything/src/main/java/com/haimin/li/jvm/MyJVM.java
@@ -27,11 +27,11 @@ public class MyJVM {
 //        Reference<Red> reference = new SoftReference<Red>(red);
 
 //        StackOverflowErrorTest();
-//        javaHeapSpaceTest();
+        javaHeapSpaceTest();
 //        GCOverheadLimitExceeded();
 //        directBufferMemory();
 //        unableToCreateNewNativeThread();
-        metaspace();
+//        metaspace();
     }
 
     private static void metaspace() {
@@ -227,15 +227,105 @@ public class MyJVM {
      *                      6）UseG1GC
      *                      7）UseSerialOldGC(被废弃)
      *              3）配置，作为参数开始 +UseConcMarkSweepGC
-     *              4）
+     *      3）7大垃圾回收器概述
+     *              1）参数描述（日志中会出现）
+     *                      1）DefNew     -> Default New Generation
+     *                      2）Tenured    -> Old
+     *                      3）ParNew     -> Parallel New Generation
+     *                      4）PSYoungGen -> Parallel Scavenge
+     *                      5）ParOldGen  -> Parallel Old Generation
+     *              2）Young区（新生代）
+     *                      1）串行GC（Serial / Serial Copying） Client模式下新生区的默认垃圾收集器
+     *                           配置 -Xms10m -Xmx10m -XX：+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseSerialGC  （DefNew + Tenured）
+     *                          开启后 Serial(Young区） + Serial Old（Old区）  表示新生代和老年代都使用串行垃圾收集器，新生代使用复制算法，老年代使用标记-整理算法
+     *                      2）并行GC(ParNew) 其实是Serial垃圾收集器在新生代的并行多行程版本 很多java虚拟机运行在server模式下新生代的默认垃圾收集器
+     *                          配置 -Xms10m -Xmx10m -XX：+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseParNewGC  （ParNew + Tenured）
+     *                          开启后 ParNew(Young区） + Serial Old（Old区）  新生代使用复制算法，老年代使用标记-整理算法
+     *                          -XX:ParallelGCThreads  限制线程数量，默认开启和cpu数目相同的线程数
+     *                      3）并行回收GC(Parallel / Parallel Scavenge) java 8 默认使用 新生代和老年代都是并行的 俗称吞吐量优先收集器 吞吐量 = 运行用户代码时间 /(运行用户代码时间 + GC时间)
+     *                          配置 -Xms10m -Xmx10m -XX：+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseParallelGC  （PSYoungGen + ParOldGen）
+     *                          自适应调节策略：虚拟机会根据当前系统的运行情况收集性能监控信息，动态调节这些参数以提供合适的停顿时间和最大的吞吐量
+     *                          开启后 PSYoungGen(Young区） + ParOldGen（Old区）  新生代使用复制算法，老年代使用标记-整理算法
      *
-     *      3）
-     *      4）
-     *      5）
+     *              3）Old区（老年代） 和新生代相互激活
+     *                      1）串行GC（Serial Old / Serial MSC）
+     *                           配置 -Xms10m -Xmx10m -XX：+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseSerialOldGC  已经无法识别
+     *                           了解即可，要废除了
+     *
+     *                      2）并行GC(Parallel Old / Parallel MSC)
+     *                          配置 -Xms10m -Xmx10m -XX：+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseParallelOldGC  （PSYoungGen + ParOldGen）
+     *                      3）并发标记清除GC（CMS） 先标记处要回收的对象，在回收，
+     *                          配置 -Xms10m -Xmx10m -XX：+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseConcMarkSweepGC  （ParNew + CMS）
+     *                          开启后 ParNew(Young区） + ParOldGen（Old区）+Serial Old   Serial Old 将作为CMS出错的后备收集器新生代使用复制算法，老年代使用标记-清楚-整理算法
+     *                          步骤：
+     *                              1）初始标记（initial mark）：只是标记一下 GC Root能直接关联的对象，速度很快，但是要暂停所有的工程线程
+     *                              2）并发标记（concurrent mark）：进行GC Root的跟踪过程，和用户线程一起执行，主要标记过程，标记所有对象
+     *                              3）重新标记（remark）：修正并发标记期间，因用户线程继续运行而导致标记产生变动的那一部分的标记记录，要暂停所有的用户线程
+     *                              4）并发清除（concurrent sweep）：清除GC Root不可达对象，和用户线程一起工作
+     *                          缺点：1）有内存碎片，使用Serial Old 进行整理  -XX:CMSFullGCSBeForeCompaction (默认 0）多少次CMS之后进行Full GC
+     *                               2）CMS必须在堆内存用完之前完成GC 否则会失败，出发担保机制，造成较大时间停顿
+     *                          优点：并发收集低停顿
+     *
+     *              4）G1（garbage first） 垃圾收集器，是一款面向服务端应用的收集器
+     *                      1）配置 -Xms10m -Xmx10m -XX：+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseG1GC
+     *                      2）上面 的垃圾收集器的特点
+     *                              1）年轻代和老年代是各自独立的且连续的内存块
+     *                              2）年轻代收集使用单Eden-s0-s1进行复制算法
+     *                              3）老年代收集必须扫描整个来年代区域
+     *                              4）都是以尽可能少而快速的执行GC为设计原则
+     *                      3）设计的目标是取代CMS，和CMS相比：
+     *                              1）G1是一个有整理内存过程的垃圾收集器，不会产生过度垃圾碎片
+     *                              2）G1的STW（stop the Word）更可控，G1 在停顿时间上添加了预测机制，用户可以指定期望的停顿时间
+     *                      4）主要改变是Eden，surviver和 tenured 内存区域不是连续的，而是变成一个个大小一样的region，每个region从1M - 32M 不等，每个region都有可能是Eden，surviver和 tenured
+     *                      5）特点：
+     *                              1）充分利用多cpu，多核环境的硬件优势，尽量缩短STW
+     *                              2）整体上采用标记整理算法，局部是采用复制算法，不会产生内存碎片
+     *                              3）宏观上G1不区分年轻代和老年代，把内存划分成多个独立的子区域（region），但是在小范围上还是区分年轻代和老年代，但是他们不是物理隔离的，
+     *                                 而是一部分region的集合且不要求region是连续的，还是会用不同的GC方式来处理不同的区域
+     *                              4）G1也是分代收集器，只是逻辑上的分代，每个region都有可能随着G1 的运行在不同的代切换
+     *              5） G1底层原理
+     *                      1） 总纲：化整为零，避免全内存扫描，只需要按照区域来扫描即可
+     *                      2） -XX:G1HeapRegionSize=2(1-32 ,2的幂)， 默认划分2048个分区  -XX:MaxGCPauseMillis=n  z最大停顿时间，jvm会尽可能的停顿小于这个时间，单位毫秒
+     *                      3） G1算法将region分为 Eden， survivor，old，humongous（大对象区）
+     *                          这些region一部分包含新生代，新生代的垃圾收集依然采用暂停所有应用线程的方式，将存活对象拷贝到老年代或者survivor空间
+     *                          这些region一部分包含老年代，G1收集器通过将对象从一个区域复制到另外一个区域，完成清理工作，完成了堆的压缩（部分堆），避免了CMS内存碎片的产生
+     *                          在G1中有一种特殊的区域，Humongous（巨大的）区，如果一个对象占用的空间超过了分区容量的50%，这些对象就会分配到老年区
+     *                      4）回收步骤，和 CMS 差不多
+     *                              1）初始标记 ：只标记GC Roots 能直接关联到的对象
+     *                              2）并发标记 ： 进行GC Roots Tracing（追踪）的过程
+     *                              3）最终标记：修正并发标记期间，因程序导运行导致的变化的对象
+     *                              4）筛选回收：根据时间来进行价值最大化的回收
+     *                              5）Eden区：Eden区耗尽后会被触发们主要是 小区域收集+形成连续的内存块避免内存碎片
+     *                                      1） Eden 区的数据移动到 survivor 区，如果 survivor 空间不够，Eden 区数据部分会晋升到 old区
+     *                                      2） survivor 区区数据移动到新的 survivor 区，部分数据会晋升到old区
+     *                                      3）最后Eden区收拾干净，GC结束
+     *                      5）和 CMS 比较
+     *                              1） G1 没有内存碎片
+     *                              2） 可以控制停顿时间，
      *
      */
     /**
-     * 8.
+     * 8. undertow 技术
+     *
+     *
+     *
+     */
+    /**
+     * 9. 服务变慢，诊断
+     *      1） 整机 ：top -> 1）可以看程序的负载； 2） load average  或者 uptime； 3）按1
+     *      2） CPU ：vmstat -> vmstat -n 2 3   每2秒采集一次，采集3次
+     *                  r： 运行和等待CPU时间片的进程数，原则上不要超过总核数的2倍，否则压力大
+     *                  b： 阻塞的进程数，比如等待磁盘IO，网络IO
+     *                  cpu->us：用户进程消耗CPU时间的百分比，长期大约50%，应该优化
+     *               mpstat -p All 2  没2秒打印一次每个cup的使用情况
+     *               pidstat -u 1 -p 进程编号 PID  没1秒 查看某个pid程序占用CPU的情况
+     *      3） 内存 ：free -> free -m  按照兆B显示内存的使用情况
+     *                        pidstat -p 进程编号（PID） -r 2  没2秒打印一次pid占用内存的情况
+     *      4） 硬盘 ：df -> df -h
+     *      5） 磁盘IO ：iostat -> iostat -xdk 2 3
+     *                  pidstat -d 采样间隔 -p 进程编号（PID）
+     *      6） 网络IO ：ifstat ->
+     *      7）
      *
      *
      *
